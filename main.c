4efc215f9dca91918886a5e606cfb1803da6f2d8
Zytron1228 <dastelljes@outlook.com>
2024-07-16 14:33:57 -0500

    merge

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
+#include <malloc.h>
 
 int main(void)
 {
-	char num[100];
+	char *num = malloc(sizeof(char) * (unsigned) -1); /* allocate OVER 4 GB OF MEMORY AAAAA*/
+
 	printf("Enter a string: ");
 	scanf("%s", &num);
 	printf("You entered: %s\n", num);
 	/* TEST */
+	free(num);
 	/* snewby changes */
 	/* branch change? */
 	return (0);
