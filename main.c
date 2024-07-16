0dff2eb09a0cf06c6085af2871c43d745cd9eca0
TheSnewby <stephen.j.newby@gmail.com>
2024-07-16 15:04:32 -0500

    notes on strtok

--- main.c
+++ main.c
@@ -19,5 +19,18 @@ int main(void)
 	free(num);
 	/* snewby changes */
 	/* branch change? */
+
+	/* first token */
+    char* tok = strtok(num, " ");
+
+        while (tok != NULL) {
+        printf(" % s\n", tok); /* prints individually - should look into how to store all values instead of print */
+		/* consider counting number of words after first word to keep track of arguments for a function */
+		/* if the number of arguments matches the number required for the command in the first argument: fork command and input arguments */
+		/* - if not either display an error, ignore the garbage words, or something else */
+		/* keep an eye out for special characters that can change the meaning */
+     
+	    tok = strtok(NULL, " "); /* NULL as an argument informs strtok to continue using previous input */
+    }
 	return (0);
 }
