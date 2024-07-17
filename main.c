d5af5fe934669e3975c0dba1b25d4d5017844013
TheSnewby <stephen.j.newby@gmail.com>
2024-07-17 13:06:58 -0500

    added more libraries to .h and added a skeleton of the fork and execve

--- main.c
+++ main.c
@@ -1,9 +1,10 @@
-#include <stdio.h>
 #include <malloc.h>
+#include "main.h"
 
 int main(void)
 {
 	char input[1024]; /* user input */
+	pid_t fork_rtn;
 
 	printf("Enter a string: ");
 	scanf("%s", &input);
@@ -11,11 +12,8 @@ int main(void)
 	printf("%s\n", input);
 	/* TEST */
 
-	/* snewby changes */
-	/* branch change? */
-
 	/* first token */
-    char* tok = strtok(num, " ");
+    char* tok = strtok(input, " ");
 
         while (tok != NULL) {
         printf(" % s\n", tok); /* prints individually - should look into how to store all values instead of print */
@@ -23,8 +21,23 @@ int main(void)
 		/* if the number of arguments matches the number required for the command in the first argument: fork command and input arguments */
 		/* - if not either display an error, ignore the garbage words, or something else */
 		/* keep an eye out for special characters that can change the meaning */
-     
+
 	    tok = strtok(NULL, " "); /* NULL as an argument informs strtok to continue using previous input */
     }
+
+	/* run user-inputed commands - skeleton version */
+	fork_rtn = fork();
+	if (fork_rtn == 0) /* child */
+	{
+		
+		execve();
+	}
+	else if (fork_rtn < 0) /* fork failed */
+		perror("fork failed");
+	else /* parent; fork_rtn contains pid of child process */
+	{
+		wait(); /* waits until child process completes */
+	}
+
 	return (0);
 }
