d2bfe1deefc5e3c9b27cd07c3efe14284b67a51a
Zytron1228 <dastelljes@outlook.com>
2024-07-19 18:34:03 -0500

    test run script added flag -ggdb3 for debugging

--- TestRunScript.bash
+++ TestRunScript.bash
@@ -1,5 +1,5 @@
 #!/bin/env bash
-gcc *.c -o testRun -Wall -Wextra -pedantic -std=gnu89
+gcc -ggdb3 *.c -o testRun -Wall -Wextra -pedantic -std=gnu89
 if ! test -f ./testRun
 then
   echo "Couldn't compile."
