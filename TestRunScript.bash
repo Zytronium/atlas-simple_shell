e029af3c90b22f944a9389160d8b9e5abfa2d7e6
zytronium <dastelljes@outlook.com>
2024-07-22 22:21:01 -0500

    moved supersimp.c into its own directory

--- TestRunScript.bash
+++ TestRunScript.bash
@@ -1,5 +1,5 @@
 #!/bin/env bash
-gcc -ggdb3 main.c shell_loop.c shellf_destruct.c -o testRun -Wall -Wextra -pedantic -std=gnu89
+gcc -ggdb3 *.c -o testRun -Wall -Wextra -pedantic -std=gnu89
 if ! test -f ./testRun
 then
   echo "Couldn't compile."
