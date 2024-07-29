1e55543c69c9ae52fc8421171e7d25f6de1c2145
zytronium <dastelljes@outlook.com>
2024-07-29 09:53:50 -0500

    added debug_script.bash

---
+++ debug_script.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+if test -f ./mix_ls_bin_ls_spaces.bash
+then
+  cat mix_ls_bin_ls_spaces.bash
+fi
+
+#note: We only plan to use this if we are told that it's allowed. This script
+# would print the contents of a specific checker script, to help us see what
+# exactly it's doing, to help us debug.
